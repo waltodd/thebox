b0091e1d4c6c28e1a509354762b691d8a4139341
waltodd <waltodd@hotmail.com>
2024-08-29 10:48:40 +0100

    carousel hero added

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,15 @@ import Footer from "@/components/Footer";
 import CTA from "@/components/CTA";
 import Ads from "@/components/Ads";
 import Hero from "@/components/Hero";
+import { EmblaOptionsType } from 'embla-carousel'
+
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       {/* <AboutUs /> */}
-      <Hero />
+      <Hero  />
       <Service />
       <Ads />
       <CTA />
--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,19 @@
-import icons from './icons'
-import images from './images'
-export {
-    icons,
-    images
-}
+import icons from "./icons";
+import images from "./images";
+
+import project1 from "@/assets/images/bg-consulting.png";
+import project2 from "@/assets/images/rock.jpg";
+export { icons, images };
+
+
+
+export const slides = [
+  {
+    projectName: "The National University of Architecture",
+    image: "https://utfs.io/f/0f13ab2a-60c7-4624-bca3-1c778611376d-2359h.jpg",
+  },
+  {
+    projectName: "The Rock and Roll Hall of Fame and Museum",
+    image: "https://utfs.io/f/92317450-7fe9-44fb-b973-675139a25fe9-1nptsa.jpg",
+  },
+];
